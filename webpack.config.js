40c1417930893e9e4aa1abd94e542bf81397144c
davcs86 <davcs86@gmail.com>
2017-12-18 18:17:54 -0600

    ..

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
 // Helper: root() is defined at the bottom
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ContextReplacementPlugin } = require('webpack');
+const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { ContextReplacementPlugin } = require('webpack');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const autoprefixer = require('autoprefixer');
+// const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 //
-// var webpack = require('webpack');
-//
 // // Webpack Plugins
-// var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
-// var autoprefixer = require('autoprefixer');
 // var HtmlWebpackPlugin = require('html-webpack-plugin');
 // var ExtractTextPlugin = require('extract-text-webpack-plugin');
-// var CopyWebpackPlugin = require('copy-webpack-plugin');
-//
 // /**
 //  * Env
 //  * Get npm lifecycle event to identify the environment
@@ -45,17 +44,17 @@ module.exports = function makeWebpackConfig() {
     //     config.devtool = 'eval-source-map';
     // }
 
-    if (!isTest) {
+    //if (!isTest) {
         /**
          * Entry
          * Reference: http://webpack.github.io/docs/configuration.html#entry
          */
-        config.entry = isTest ? {} : {
-            'polyfills': './frontend/app/polyfills.ts',
-            'vendor': './frontend/app/vendor.ts',
-            'app': './frontend/app/main.ts' // our angular app
-        };
-    }
+    config.entry = {
+        'polyfills': './frontend/app/polyfills.ts',
+        'vendor': './frontend/app/vendor.ts',
+        'app': './frontend/app/main.ts' // our angular app
+    };
+    //}
 
     /**
      * Output
@@ -90,8 +89,8 @@ module.exports = function makeWebpackConfig() {
             // Support for .ts files.
             {
                 test: /\.ts$/,
-                loaders: ['@angularclass/hmr-loader', 'awesome-typescript-loader?' + atlOptions, 'angular2-template-loader'],
-                exclude: [isTest ? /\.(e2e)\.ts$/ : /\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/]
+                loaders: ['awesome-typescript-loader?' + atlOptions],
+                exclude: [/\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/]
             },
 
             // copy those assets to output
@@ -101,7 +100,9 @@ module.exports = function makeWebpackConfig() {
             // },
 
             // Support for *.json files.
-            {test: /\.json$/, loader: 'json-loader'},
+            {
+                test: /\.json$/, loader: 'json-loader'
+            },
 
             // Support for CSS as raw text
             // use 'null' loader in test mode (https://github.com/webpack/null-loader)
@@ -115,25 +116,33 @@ module.exports = function makeWebpackConfig() {
                 })
             },
             // all css required in src/app files will be merged in js files
-            {test: /\.css$/, include: root('src', 'app'), loader: 'raw-loader'},
+            {
+                test: /\.css$/,
+                include: root('src', 'app'),
+                loader: 'raw-loader'
+            },
 
             // support for .scss files
             // use 'null' loader in test mode (https://github.com/webpack/null-loader)
             // all css in src/style will be bundled in an external css file
             {
                 test: /\.(scss|sass)$/,
-                exclude: root('src', 'app'),
+                //exclude: root('src', 'app'),
                 loader: isTest ? 'null-loader' : ExtractTextPlugin.extract({
                     fallback: 'style-loader',
                     use: ['css-loader', 'sass-loader']
                 })
             },
             // all css required in src/app files will be merged in js files
-            {test: /\.(scss|sass)$/, exclude: root('src', 'style'), loader: 'raw-loader!sass-loader'},
+            //{test: /\.(scss|sass)$/, exclude: root('src', 'style'), loader: 'raw-loader!sass-loader'},
 
             // support for .html as raw text
             // todo: change the loader to something that adds a hash to images
-            {test: /\.html$/, loader: 'raw-loader', exclude: root('src', 'public')}
+            {
+                test: /\.html$/,
+                loader: 'raw-loader',
+                exclude: root('src', 'public')
+            }
         ]
     };
 
